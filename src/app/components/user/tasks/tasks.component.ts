7283627d418caa56baf9b9ab5c6788ec4519d9ed
Roman Nemchinov <nemchinovrp@yandex.ru>
2019-12-26 17:22:12 +0300

    refactoring dadata service

--- src/app/components/user/tasks/tasks.component.ts
+++ src/app/components/user/tasks/tasks.component.ts
@@ -1,32 +1,18 @@
-import {
-  Component,
-  ElementRef,
-  EventEmitter,
-  HostListener,
-  Input,
-  OnChanges,
-  OnInit,
-  Output,
-  Renderer2,
-  SimpleChanges,
-  ViewChild
-} from '@angular/core';
+import {Component, ElementRef, EventEmitter, Input, OnInit, Output, Renderer2, ViewChild} from '@angular/core';
 //
 import {DadataService, DadataType} from '../../../services/dadata.service';
 import {DadataSuggestion} from '../../../model/dadata/suggestion';
-import {ControlValueAccessor} from '@angular/forms';
 import {Subject, timer} from 'rxjs';
 import {DadataConfig, DadataConfigDefault} from '../../../model/dadata/dadata-config';
 import {DadataResponse} from '../../../model/dadata/dadata-response';
 import {debounce} from 'rxjs/operators';
-import {DadataAddress} from '../../../model/dadata/data';
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.css']
 })
-export class TasksComponent implements OnInit, ControlValueAccessor, OnChanges {
+export class TasksComponent implements OnInit {
   private v: any = '';
   data: DadataSuggestion[] = [];
   currentFocus = -1;
@@ -36,11 +22,11 @@ export class TasksComponent implements OnInit, ControlValueAccessor, OnChanges {
   @Input() config: DadataConfig = DadataConfigDefault;
   @Output() selectedSuggestion: DadataSuggestion;
   @Output() selected: EventEmitter<DadataSuggestion> = new EventEmitter<DadataSuggestion>();
-  propagateChange: any = () => {};
+  propagateChange: any = () => {
+  };
 
   // @ts-ignore
-  @ViewChild('inputValue', { static: true }) inputValue: ElementRef;
-  onTouched = () => {};
+  @ViewChild('inputValue', {static: true}) inputValue: ElementRef;
 
   public inputString$ = new Subject<string>();
 
@@ -59,78 +45,38 @@ export class TasksComponent implements OnInit, ControlValueAccessor, OnChanges {
   }
 
   getData(value: string) {
-    console.log(value);
+    console.log('getData Method - ', value);
     this.inputString$.next(value);
     this.currentFocus = -1;
   }
 
   onClick(e: MouseEvent, item: DadataSuggestion) {
-    // e.preventDefault();
+    console.log('onClick Start', e);
     this.inputValue.nativeElement.value = item.value;
     this.propagateChange(item.value);
     this.inputValue.nativeElement.focus();
     this.selectedSuggestion = item;
     this.data = [];
     this.currentFocus = -1;
-
-    // this.writeValue(item.value);
+    console.log('onClick DadataSuggestion', item);
     this.selected.emit(item);
-    // this.selectedData.emit(item.data);
-    // this.selectedString.emit(item.value);
   }
 
   onEnter() {
-    console.log('onEnter', this.selectedSuggestion, this.data[this.currentFocus]);
+    console.log('onEnterMethod start');
     this.selectedSuggestion = this.data[this.currentFocus];
+    console.log('onEnterMethod this.selectedSuggestion', this.selectedSuggestion);
+    console.log('onEnterMethod this.data[this.currentFocus]', this.data[this.currentFocus]);
     this.inputValue.nativeElement.value = this.selectedSuggestion.value;
+    console.log('onEnterMethod this.inputValue.nativeElement.value', this.inputValue.nativeElement.value);
     this.data = [];
     this.currentFocus = -1;
     this.propagateChange(this.selectedSuggestion.value);
-    // this.writeValue(this.selectedSuggestion.value);
     this.selected.emit(this.selectedSuggestion);
-    // this.selectedData.emit(this.selectedSuggestion.data);
-    // this.selectedString.emit(this.selectedSuggestion.value);
-  }
-
-  onArrowDown() {
-    this.removeFocus(this.currentFocus);
-    if (this.currentFocus >= this.data.length - 1) {
-      this.currentFocus = 0;
-    } else {
-      this.currentFocus++;
-    }
-    this.setFocus(this.currentFocus);
-  }
-
-  onArrowUp() {
-    this.removeFocus(this.currentFocus);
-    if (this.currentFocus === 0) {
-      this.currentFocus = this.data.length - 1;
-    } else {
-      this.currentFocus--;
-    }
-    this.setFocus(this.currentFocus);
-  }
-
-  setFocus(id: number) {
-    const activeEl = document.getElementById(id + 'item');
-    this.r.addClass(activeEl, 'active');
-  }
-
-  removeFocus(id: number) {
-    if (id !== -1) {
-      const activeEl = document.getElementById(id + 'item');
-      this.r.removeClass(activeEl, 'active');
-    }
-  }
-
-  ngOnChanges(changes: SimpleChanges): void {
   }
 
   ngOnInit() {
     this.type = this.config.type;
-/*    this.locations = this.config.locations;
-    this.dataService.setApiKey(this.apiKey ? this.apiKey : this.config.apiKey);*/
     this.inputString$.pipe(
       debounce(() => timer(this.config.delay ? this.config.delay : 500)),
     ).subscribe(x => {
@@ -141,24 +87,5 @@ export class TasksComponent implements OnInit, ControlValueAccessor, OnChanges {
         console.log('ngOnInit - y.suggestions', y.suggestions);
       });
     });
-  }
-
-  registerOnChange(fn: any): void {
-    this.propagateChange = fn;
-  }
-
-  registerOnTouched(fn: any): void {
-    this.onTouched = fn;
-  }
-
-  writeValue(value: any): void {
-    if (value !== undefined) {
-      this.v = value;
-    }
-  }
-
-  @HostListener('document:click')
-  onOutsideClick() {
-    this.data = [];
   }
 }
