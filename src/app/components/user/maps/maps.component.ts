0ffa706c5e5a6d37461759cfb21aaca1454ce957
Roman Nemchinov <nemchinovrp@yandex.ru>
2019-12-30 15:25:19 +0300

    Добавил рабочие карты

--- src/app/components/user/maps/maps.component.ts
+++ src/app/components/user/maps/maps.component.ts
@@ -1,18 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {animationFrameScheduler, interval, Subscription} from 'rxjs';
+import {observeOn} from 'rxjs/operators';
 
 @Component({
   selector: 'app-maps',
   templateUrl: './maps.component.html',
   styleUrls: ['./maps.component.css']
 })
-export class MapsComponent implements OnInit {
-  title = 'My first AGM project';
-  lat = 51.678418;
-  lng = 7.809007;
+export class MapsComponent implements OnInit, OnDestroy{
+  center = [0, 0];
+
+  private sub: Subscription;
 
   constructor() { }
 
   ngOnInit() {
+    this.sub = interval(100).pipe(
+      observeOn(animationFrameScheduler)
+    ).subscribe(() => {
+      if (this.center[0] >= 180) {
+        this.center = [-this.center[0], 0];
+      } else {
+        this.center = [this.center[0] + 1, 0];
+      }
+    });
   }
 
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
 }
